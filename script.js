100aa001bfabeedfde21c28c61a8258e3de16d8f
Matheus de Gondra <matheusgondra951@gmail.com>
2024-01-02 22:29:40 -0300

    refactor: add label in input questions

--- script.js
+++ script.js
@@ -653,7 +653,10 @@ function displayQuestions() {
         const questionElement = document.createElement("div");
         questionElement.innerHTML = `
             <h3>Pergunta ${index + 1}: ${question.question}</h3>
-            ${question.options.map(option => `<input type="radio" name="question${index}" value="${option}">${option}<br>`).join("")}
+            ${question.options.map(option => `
+				<input type="radio" id="${option}" name="question${index}" value="${option}">
+				<label for="${option}">${option}</label><br>
+				`).join("")}
             <hr>
         `;
         questionsContainer.appendChild(questionElement);
